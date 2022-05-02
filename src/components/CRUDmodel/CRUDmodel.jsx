f08716918d2f08d7680b3c10217cb385cec455cd
DavidHorvitz <2205dzh@gmail.com>
2022-05-02 23:35:07 +0300

    1

---
+++ src/components/CRUDmodel/CRUDmodel.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import classes from './CRUDmodel.css';
+
+const CRUDmodel = () => (
+    <div className="modelCrud">
+
+        <div >
+            <input type="text" />     Product name
+        </div>
+        <div>
+            <input type="text" />     Department
+        </div>
+        <div>
+            <input type="text" />  Price
+        </div>
+        <div>
+            <input type="text" />    Product Id
+        </div>
+        <button>ssssssss</button>
+    </div>
+);
+export default CRUDmodel;
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from './Layout.css';
+import CRUDmodel from "../CRUDmodel/CRUDmodel";
+
+
+const Layout = (props) => (
+    <div>
+        <h1 >Online shop Management</h1>
+        <CRUDmodel/>
+    </div>
+
+) ;
+export default Layout;
