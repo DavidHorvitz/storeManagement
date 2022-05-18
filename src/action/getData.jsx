c19fd5de7f9a5a792fca8713fc41ec84328ea13d
DavidHorvitz <2205dzh@gmail.com>
2022-05-19 00:24:12 +0300

    4

--- src/action/getData.jsx
+++ src/action/getData.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const GET_ALL_PRODUCT = 'GET_ALL_PRODUCT'
 
 export const getAllProducts = () => async dispatch => {
-  axios.get('http://192.168.17.142:3010/api/get-products')
+  axios.get('http://192.168.100.3:3010/api/get-products')
     .then(response => {
       dispatch({ type: GET_ALL_PRODUCT, payload: response.data });
     })
--- src/action/setData.jsx
+++ src/action/setData.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const SET_PRODUCT = 'SET_PRODUCT'
 
 export const creatProduct = obj => async dispatch => {
-  axios.post('http://192.168.17.142:3010/api/create-product',{
+  axios.post('http://192.168.100.3:3010/api/create-product',{
     data: obj
   })
     .then(response => {
